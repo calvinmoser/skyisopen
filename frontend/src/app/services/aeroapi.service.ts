cff0d56e7746e8dc89dbe802cf81a76fc48e1939
Calvin Moser <cmoser@techietable.com>
2024-02-15 15:23:09 -0500

    Use secure /aero endpoints.

--- frontend/src/app/services/aeroapi.service.ts
+++ frontend/src/app/services/aeroapi.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Flight, Position } from '../model/flight';
 
 @Injectable({
@@ -11,18 +11,25 @@ export class AeroAPIService{
 
   constructor(private httpClient: HttpClient) {}
 
+  readonly headers = new HttpHeaders({
+          authorization : 'Basic ' + btoa("user" + ':' + "password")
+      });
 
   getScheduledArrivals(maxPages: number): Observable<Flight[]> {
-    const options = maxPages ? { params: new HttpParams().set('maxPages', maxPages) } : {};
-    return this.httpClient.get<Flight[]>("/scheduled_arrivals", options)
+    const params = maxPages ? new HttpParams().set('maxPages', maxPages) : {};
+    const options = { params: params, headers: this.headers };
+
+    return this.httpClient.get<Flight[]>("/aero/scheduled_arrivals", options)
       .pipe(
         map(flights => flights.map(flight => new Flight(flight)))
       );
   }
 
   getFlightPosition(fa_flight_id: string): Promise<Flight | undefined> {
-    var flight_position_url = "/flights/" + fa_flight_id + "/position";
-    return this.httpClient.get<Flight>(flight_position_url)
+    const options = { headers: this.headers };
+    var flight_position_url = "/aero/flights/" + fa_flight_id + "/position";
+
+    return this.httpClient.get<Flight>(flight_position_url, options)
       .pipe(
         map(last_position => new Flight(last_position))
       ).toPromise();
