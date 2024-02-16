2b503d1b5a4d61958e3b5d4cf26410f6541c0ad6
Calvin Moser <cmoser@techietable.com>
2024-02-16 15:19:05 -0500

    Use basic auth for browser sign-in

--- backend/src/main/java/com/techietable/skyisopen/controller/WebSecurityConfig.java
+++ backend/src/main/java/com/techietable/skyisopen/controller/WebSecurityConfig.java
@@ -4,14 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -21,9 +25,10 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/aero").authenticated()
+                .requestMatchers("/aero/**").authenticated()
                 .anyRequest().permitAll()
-            );
+                )
+            .httpBasic(withDefaults());
 
         return http.build();
     }
