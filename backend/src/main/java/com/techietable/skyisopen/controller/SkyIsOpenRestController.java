83e42cca8953e5a4644276785aade7aed0ae63a7
Calvin Moser <cmoser@techietable.com>
2026-05-29 12:00:00 -0400

    Add logging to controller.

--- backend/src/main/java/com/techietable/skyisopen/controller/SkyIsOpenRestController.java
+++ backend/src/main/java/com/techietable/skyisopen/controller/SkyIsOpenRestController.java
@@ -2,12 +2,14 @@ package com.techietable.skyisopen.controller;
 
 import com.techietable.skyisopen.dto.Flight;
 import com.techietable.skyisopen.service.SkyisOpenServiceLayer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/aero")
 public class SkyIsOpenRestController {
@@ -17,33 +19,43 @@ public class SkyIsOpenRestController {
 
     @GetMapping("/scheduled_arrivals")
     public synchronized ArrayList<Flight> getScheduledFlights(@RequestParam(required = false) Integer maxPages) {
+        log.debug("getScheduledFlights: maxPages={}", maxPages);
 
         if (maxPages == null)
             maxPages = 1;
         else if (maxPages < 1 || maxPages > 10)
             throw new SkyIsExceptional(HttpStatus.BAD_REQUEST, "max_pages must be between 1 and 10 inclusively");
 
-        return service.scheduledFlights(maxPages);
+        ArrayList<Flight> flights = service.scheduledFlights(maxPages);
+        log.debug("getScheduledFlights: returning {} flights", flights.size());
+        return flights;
     }
 
     @GetMapping("/flights/search")
-    public synchronized ArrayList<Flight> searchAreaForPlanes (
+    public synchronized ArrayList<Flight> searchAreaForPlanes(
         @RequestParam(required = false) Integer maxPages)
     {
+        log.debug("searchAreaForPlanes: maxPages={}", maxPages);
+
         if (maxPages == null)
             maxPages = 1;
         else if (maxPages < 1 || maxPages > 10)
             throw new SkyIsExceptional(HttpStatus.BAD_REQUEST, "max_pages must be between 1 and 10 inclusively");
 
-        return service.searchAreaForPlanes(maxPages);
+        ArrayList<Flight> flights = service.searchAreaForPlanes(maxPages);
+        log.debug("searchAreaForPlanes: returning {} flights", flights.size());
+        return flights;
     }
 
     @GetMapping("/flights/{id}/position")
     public synchronized Flight getFlightPosition(@PathVariable String id) {
+        log.debug("getFlightPosition: id={}", id);
 
-        if (id == null || id == "")
+        if (id == null || id.isEmpty())
             throw new SkyIsExceptional(HttpStatus.BAD_REQUEST, "id not specified");
 
-        return service.flightPosition(id);
+        Flight flight = service.flightPosition(id);
+        log.debug("getFlightPosition: returning position for id={}", id);
+        return flight;
     }
 }
