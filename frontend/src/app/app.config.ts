b4d7315b3f0ac40c5830f2ce2a15b2f3c05b10f6
Calvin Moser <cmoser@techietable.com>
2026-05-29 10:16:23 -0400

    Add logging service.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,22 @@
-import { ApplicationConfig,  importProvidersFrom} from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    provideAnimations(),
+    importProvidersFrom(
+      LoggerModule.forRoot({
+        level: isDevMode() ? NgxLoggerLevel.DEBUG : NgxLoggerLevel.ERROR,
+      })
+    )
+  ]
 };
--- frontend/src/app/services/aeroapi.service.ts
+++ frontend/src/app/services/aeroapi.service.ts
@@ -1,32 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Flight, Position } from '../model/flight';
+import { NGXLogger } from 'ngx-logger';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AeroAPIService{
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private logger: NGXLogger) {}
 
   getScheduledArrivals(maxPages: number): Observable<Flight[]> {
     const params = maxPages ? new HttpParams().set('maxPages', maxPages) : {};
     const options = { params: params };
 
+    this.logger.debug(`getScheduledArrivals: requesting ${maxPages} page(s)`);
+
     return this.httpClient.get<Flight[]>("/aero/scheduled_arrivals", options)
       .pipe(
-        map(flights => flights.map(flight => new Flight(flight)))
+        map(flights => flights.map(flight => new Flight(flight))),
+        tap(flights => this.logger.debug(`getScheduledArrivals: received ${flights.length} flights`))
       );
   }
 
   getFlightPosition(fa_flight_id: string): Promise<Flight | undefined> {
     var flight_position_url = "/aero/flights/" + fa_flight_id + "/position";
 
+    this.logger.debug(`getFlightPosition: requesting position for ${fa_flight_id}`);
+
     return this.httpClient.get<Flight>(flight_position_url)
       .pipe(
-        map(last_position => new Flight(last_position))
+        map(last_position => new Flight(last_position)),
+        tap(flight => this.logger.debug(`getFlightPosition: received position for ${fa_flight_id}`))
       ).toPromise();
   }
 
